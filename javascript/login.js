d101ed50bf9bd7d0b8aca175f9d4a4bd422cd0e6
Nina <ninawedig@yahoo.de>
2024-03-03 19:53:34 +0100

    New files

---
+++ javascript/login.js
@@ -0,0 +1,14 @@
+const activeUserButton = document.getElementById('activeUserButton');
+const smallMenu = document.getElementById('smallMenu');
+
+function init(){
+    renderHeader();
+    renderNavbar();
+    makeNavbarActive('summary');
+    
+}
+
+function showSmallMenu(){
+    smallMenu.classList.toggle('noDisplay');
+}
+

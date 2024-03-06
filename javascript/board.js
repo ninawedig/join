b97afccc10bd1775bf6caa40c8446843aff9a952
Kai Heimerich <kai.heimerich@eufh-mail.de>
2024-03-06 20:55:42 +0100

    add board and delete contact json

--- javascript/board.js
+++ javascript/board.js
@@ -6,6 +6,7 @@ function init(x){
 }
 
 function showCardDetail(){
+    renderCardDetail();
     document.getElementById('cardContainer').style = "display: flex";
     document.getElementById('cardContainerBackground').style = "display: flex";
     
@@ -39,7 +40,7 @@ function renderBoard(){
 
 function generateCard(element){
     return /*html*/`
-        <div class="taskCard">
+        <div class="taskCard" onclick="showCardDetail()">
             <div class="taskCategory ${element['category']}">${element['category']}</div>
             <div class="taskInfo">
                 <div class="taskTitle">${element['title']}</div>
@@ -83,4 +84,83 @@ function renderSubtasks(){
 
 function renderAssignTo(){
     
+}
+
+function renderCardDetail(){
+
+    document.getElementById('cardDetail').innerHTML ='';
+    document.getElementById('cardDetail').innerHTML = /*html*/`
+     <div class="cardDetailHeader">
+                <div class="taskCategory userStory">User Story</div>
+                <div class="closeIconContainer" onclick="closeCardDetail()"><img class="closeIcon"
+                    src="./img/contacts/close.svg" alt=""></div>                    
+            </div>
+            <div class="taskInfoDetail">
+                <div class="taskTitleDetail">Kochwelt Page & Recipe Recommender</div>
+                <p class="taskDescriptionDetail">Build start page with recipe recommendation...</p>
+                <table>
+                    <tr>
+                        <th>Due date:</th>
+                        <td><p>10/05/2023</p></td>
+                    </tr>
+                    <tr>
+                        <th>Priority:</th>
+                        <td style="display: flex; align-items: center; gap: 8px">
+                            <p>Medium</p>
+                            <svg width="18" height="8" viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_146349_2583)">
+                            <path d="M16.5685 7.16658L1.43151 7.16658C1.18446 7.16658 0.947523 7.06773 0.772832 6.89177C0.598141 6.71581 0.5 6.47716 0.5 6.22831C0.5 5.97947 0.598141 5.74081 0.772832 5.56485C0.947523 5.38889 1.18446 5.29004 1.43151 5.29004L16.5685 5.29004C16.8155 5.29004 17.0525 5.38889 17.2272 5.56485C17.4019 5.74081 17.5 5.97947 17.5 6.22831C17.5 6.47716 17.4019 6.71581 17.2272 6.89177C17.0525 7.06773 16.8155 7.16658 16.5685 7.16658Z" fill="#FFA800"/>
+                            <path d="M16.5685 2.7098L1.43151 2.7098C1.18446 2.7098 0.947523 2.61094 0.772832 2.43498C0.598141 2.25902 0.5 2.02037 0.5 1.77152C0.5 1.52268 0.598141 1.28403 0.772832 1.10807C0.947523 0.932105 1.18446 0.833252 1.43151 0.833252L16.5685 0.833252C16.8155 0.833252 17.0525 0.932105 17.2272 1.10807C17.4019 1.28403 17.5 1.52268 17.5 1.77152C17.5 2.02037 17.4019 2.25902 17.2272 2.43498C17.0525 2.61094 16.8155 2.7098 16.5685 2.7098Z" fill="#FFA800"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_146349_2583">
+                            <rect width="17" height="6.33333" fill="white" transform="translate(0.5 0.833252)"/>
+                            </clipPath>
+                            </defs>
+                            </svg>
+                        </td>
+                    </tr>                                            
+                </table>
+                <table><th>Assign to:</th></table>
+                <ul>
+                    <li>
+                        <div class="taskMember" style="z-index: 1;">KH</div>
+                        <p>Kai Heimerich</p>
+                    </li>
+                    <li>
+                        <div class="taskMember" style="background-color: #1FD7C1;">NW</div>
+                        <p>Nina Wedig</p>
+                    </li>
+                    <li>
+                        <div class="taskMember" style="background-color: #FF4646;">AE</div>
+                        <p>Alex Eremie</p>
+                    </li>
+                </ul>
+                <table><th>Subtasks</th></table>
+                <ul>
+                    <li>
+                        <div id="subtask0">
+                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.6821 8.39673V14.3967C17.6821 16.0536 16.339 17.3967 14.6821 17.3967H4.68213C3.02527 17.3967 1.68213 16.0536 1.68213 14.3967V4.39673C1.68213 2.73987 3.02527 1.39673 4.68213 1.39673H12.6821" stroke="#2A3647" stroke-width="2" stroke-linecap="round"/>
+                            <path d="M5.68213 9.39673L9.68213 13.3967L17.6821 1.89673" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>                        
+                        <p>Implement Recipe Reccommendation</p>    
+                    </li>
+                    <li>
+                        <div id="subtask1">
+                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="1.68213" y="1.39673" width="16" height="16" rx="3" stroke="#2A3647" stroke-width="2"/>
+                            </svg>
+                        </div>
+                        <p>Start page layout</p>    
+                    </li>
+                </ul>
+            </div>
+            <div class="taskFunctionsContainer">
+                <div class="taskFunctions"><img class="taskFunctionsIcons"
+                        src="./img/contacts/edit.svg" alt="">Edit</div>
+                <div class="taskFunctions" style="border-left: solid 1px #D1D1D1; padding-left: 16px;"><img class="taskFunctionsIcons"
+                        src="./img/contacts/delete.svg" alt="">Delete</div>
+            </div>`;
 }

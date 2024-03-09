27eca458ac7152e532fb0c079a03c88a2d6ba5e0
Nina <ninawedig@yahoo.de>
2024-03-09 08:41:44 +0100

    New Changes

--- javascript/login.js
+++ javascript/login.js
@@ -1,14 +1,1 @@
-const activeUserButton = document.getElementById('activeUserButton');
-const smallMenu = document.getElementById('smallMenu');
-
-function init(){
-    renderHeader();
-    renderNavbar();
-    makeNavbarActive('summary');
-    
-}
-
-function showSmallMenu(){
-    smallMenu.classList.toggle('noDisplay');
-}
 

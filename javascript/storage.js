c7b35574a89cdcc0f63bb8a42cd5a85e5266c87d
Alex Eremie <alex.eremie19@gmail.com>
2024-03-09 23:56:39 +0100

    added storage.js

--- javascript/storage.js
+++ javascript/storage.js
@@ -11,9 +11,3 @@ async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json());
 }
-
-async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
-}

0bb404f46846f7838800e5116b8d2cb4b4282f4e
Nina <ninawedig@yahoo.de>
2024-03-10 08:11:00 +0100

    New Changes

--- javascript/signup.js
+++ javascript/signup.js
@@ -2,7 +2,7 @@ function showSuccess(event) {
     event.preventDefault(); // Verhindert das Standardverhalten des Formulars
     
     document.getElementById('successBackgroud').classList.remove('successBackgroundHide');
-    document.getElementById('successButton').classList.remove('successButtonHide');
-    document.getElementById('successBackgroud').classList.add('successBackground');
+    document.getElementById('successBackgroud').classList.add('successBackgroundShow');
     document.getElementById('successButton').classList.add('successButtonShow');
+    document.getElementById('successButton').classList.remove('successButtonHide');
 }

7027ff2b5079db35a8a98c23485310e6ded3ca8c
Nina <ninawedig@yahoo.de>
2024-03-10 16:00:10 +0100

    New Changes

--- javascript/login.js
+++ javascript/login.js
@@ -10,25 +10,24 @@ function openLegalNoticePage() {
     window.location.href = "legalnotice.html";
 }
 
-// Definieren der Benutzerdaten
 const users = [
     { 'email': 'ninawedig@yahoo.de', 'password': 'passwort123' }
 ];
 
-// Login-Funktion
 function login() {
     let emailInput = document.getElementById('email');
     let passwordInput = document.getElementById('password');
     let messageBox = document.getElementById('messagebox');
+    let messageBoxPassword = document.getElementById('messageboxPassword');
     if (emailInput.value && passwordInput.value) {
         let user = users.find(u => u.email === emailInput.value && u.password === passwordInput.value);
         if (user) {
             openSummaryPage();
         } else {
-            messageBox.textContent = "Wrong password Ups! Try again.";
+            messageBoxPassword.textContent = "Wrong password Ups! Try again.";
         }
     } else {
-        messageBox.textContent = "Please fill in all fields.";
+        messageBox.textContent = "Please fill out this field.";
     }
 }
 function openSummaryPage() {
--- javascript/signup.js
+++ javascript/signup.js
@@ -1,37 +1,87 @@
-function init(){
+function init() {
 
 }
-async function showSuccess(event) {
-    event.preventDefault(); 
+
+let users = [
+    { 'name': 'Nina', 'email': 'ninawedig@yahoo.de', 'password': 'passwort123' }
+];
+
+
+function addUser() {
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmpassword').value;
+
+
+    let messageBoxName = document.getElementById('messageboxName');
+    let messageBoxEmail = document.getElementById('messageboxEmail');
+    let messageBoxPassword = document.getElementById('messageboxPassword');
+    let messageBoxPasswordConfirmed = document.getElementById('messageboxPasswordConfirmed');
+
+
+    if (name && email && password && confirmPassword) {
+
+        if (password !== confirmPassword) {
+
+            messageBoxPasswordConfirmed.textContent = "Ups! Your password don`t match";
+            return;
+        } else {
+            messageBoxPassword.textContent = "";
+        }
+
+
+        users.push({ 'name': name, 'email': email, 'password': password });
+        showSuccess();
+    } else {
+        if (!name) {
+            messageBoxName.textContent = "Please fill out this field.";
+        } else {
+            messageBoxName.textContent = "";
+        }
+        if (!email) {
+            messageBoxEmail.textContent = "Please fill out this field.";
+        } else {
+            messageBoxEmail.textContent = "";
+        }
+        if (!password) {
+            messageBoxPassword.textContent = "Please fill out this field.";
+        } else {
+            messageBoxPassword.textContent = "";
+        }
+        if (!confirmPassword) {
+            messageBoxPasswordConfirmed.textContent = "Please fill out this field.";
+        } else {
+            messageBoxPassword.textContent = "";
+        }
+    }
+}
+
+
+async function showSuccess() {
     document.getElementById('successBackgroud').classList.remove('successBackgroundHide');
     document.getElementById('successBackgroud').classList.add('successBackgroundShow');
     document.getElementById('successButton').classList.add('successButtonShow');
     document.getElementById('successButton').classList.remove('successButtonHide');
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms)); // Funktion zum Verzögern der Aktionen
-    await delay(2000); // Verzögertes Ausblenden der Erfolgsmeldung
+
+
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    await delay(2000);
+
     window.location.href = "login.html";
 }
 
 
 
-users = [
-    {'name': 'Nina', 'e-mail': 'ninawedig@yahoo.de', 'password': 'passwort123' } // soll nicht wie hier im Frontend gespeichert sein, sondern im Backend
-]
-
-function addUser() {
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    users.push({name: name.value, email: email.value, password: password.value});
-    window.location.href = 'login.html'; //hiermit wird man nach Registrierung zur login.html weitergeleitet
-}
-
 function openPrivacyPolicyPage() {
     window.location.href = "privacypolicy.html";
 }
 
+
 function openLegalNoticePage() {
-    window.location.href = "legalnotice.html"; 
+    window.location.href = "legalnotice.html";
 }
 
 function openLoginPage() {

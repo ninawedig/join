59e49cd10bb2bbc3d464fdd9a23fda58f8df721b
Nina <ninawedig@yahoo.de>
2024-03-10 10:17:15 +0100

    New Changes

--- javascript/login.js
+++ javascript/login.js
@@ -1,1 +1,30 @@
+function openSignUpPage() {
+    window.location.href = "signup.html";
+}
+
+function openPrivacyPolicyPage() {
+    window.location.href = "privacypolicy.html";
+}
+
+function openLegalNoticePage() {
+    window.location.href = "legalnotice.html";
+}
+
+users = [
+    {'e-mail': 'ninawedig@yahoo.de', 'password': 'passwort123' } // soll nicht wie hier im Frontend gespeichert sein, sondern im Backend
+]
+users = [
+    {'e-mail': 'ninawedig@yahoo.de', 'password': 'passwort123' } // soll nicht wie hier im Frontend gespeichert sein, sondern im Backend
+]
+
+function login() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let user = users.find(u => u.email == email.value && u.password ==  password.value);
+    console.log(user);
+    if(user) {
+        console.log('User gefunden');
+    }
+}
+
 
--- javascript/signup.js
+++ javascript/signup.js
@@ -1,3 +1,7 @@
+function init(){
+
+}
+
 function showSuccess(event) {
     event.preventDefault(); // Verhindert das Standardverhalten des Formulars
     
@@ -5,4 +9,30 @@ function showSuccess(event) {
     document.getElementById('successBackgroud').classList.add('successBackgroundShow');
     document.getElementById('successButton').classList.add('successButtonShow');
     document.getElementById('successButton').classList.remove('successButtonHide');
+
+
+}
+
+users = [
+    {'name': 'Nina', 'e-mail': 'ninawedig@yahoo.de', 'password': 'passwort123' } // soll nicht wie hier im Frontend gespeichert sein, sondern im Backend
+]
+
+function addUser() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    users.push({name: name.value, email: email.value, password: password.value});
+    window.location.href = 'login.html'; //hiermit wird man nach Registrierung zur login.html weitergeleitet
+}
+
+function openPrivacyPolicyPage() {
+    window.location.href = "privacypolicy.html";
+}
+
+function openLegalNoticePage() {
+    window.location.href = "legalnotice.html"; 
+}
+
+function openLoginPage() {
+    window.location.href = "login.html";
 }

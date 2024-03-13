cd58203de73f1beb7db110e31ec245f30dbbeb08
Alex Eremie <alex.eremie19@gmail.com>
2024-03-13 19:16:06 +0100

    Add task page. Changed Drop down menus

--- javascript/addtask.js
+++ javascript/addtask.js
@@ -1,13 +1,16 @@
 const smallMenu = document.getElementById('smallMenu');
+let contacts = [];
 
-function init(){
+async function init() {
     renderHeader();
     renderNavbar();
     makeNavbarActive('addTask');
     makeSmallNavbarActive('addTaskSmall');
+    await loadContacts();
+    renderContacts();
 }
 
-function showSmallMenu(){
+function showSmallMenu() {
     smallMenu.classList.toggle('noDisplay');
 }
 
@@ -21,29 +24,29 @@ function addtask() {
 
     if (!title.value) {
         messageBoxTitle.textContent = "Please fill out this field.";
-        title.classList.add('inputEmpty'); 
-        return; 
+        title.classList.add('inputEmpty');
+        return;
     } else {
         messageBoxTitle.textContent = "";
     }
 
     if (!duedate.value) {
         messageBoxDuedate.textContent = "Please fill out this field.";
-        duedate.classList.add('inputEmpty');  
-        return; 
+        duedate.classList.add('inputEmpty');
+        return;
     } else {
-        messageBoxDuedate.textContent = ""; 
+        messageBoxDuedate.textContent = "";
     }
 
-    if (category.value === 'x') { 
+    if (category.value === 'x') {
         messageBoxCategory.textContent = "Please select a category.";
-        category.classList.add('inputEmpty'); 
-        return; 
+        category.classList.add('inputEmpty');
+        return;
     } else {
-        messageBoxCategory.textContent = ""; 
+        messageBoxCategory.textContent = "";
     }
 
-    
+
 }
 
 function resetOutline(id) {
@@ -60,3 +63,47 @@ function clearFields() {
     document.getElementById('subtasks').value = "";
 }
 
+function toggleDropDownMenu() {
+    const arrow = document.getElementById('categoryDropDownArrow');
+    const dropDown = document.getElementById('dropDownCategory');
+
+    arrow.classList.toggle('rotated');
+    dropDown.classList.toggle('noDisplay');
+}
+
+async function loadContacts() {
+    firstLetters = await getItem('firstLetters')
+        .then(response => JSON.parse(response.data.value));
+    console.log('the first letters are', firstLetters);
+    contacts = await getItem('contacts')
+        .then(response => JSON.parse(response.data.value));
+    console.log('the contacts are', contacts);
+}
+
+function renderContacts() {
+    let contactsList = document.getElementById('contactsList');
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+
+        contactsList.innerHTML += `
+                                <div class="dropDownContact">
+                                    <div class="contactDetails">
+                                        <div class="contactProfileBadge">SM</div>
+                                        <div class="contactName">${contact.name}</div>
+                                    </div>
+                                    <img src="./img/addtask/rectangle.svg" class="checkbox">
+                                </div>
+                                
+    `;
+    }
+}
+
+function openContactsDropDown(){
+    const dropDownMenu = document.getElementById('contactsDropDownMenuContainer');
+    dropDownMenu.classList.remove('noDisplay');
+}
+
+function closeContactsDropDown(){
+    const dropDownMenu = document.getElementById('contactsDropDownMenuContainer');
+    dropDownMenu.classList.add('noDisplay');
+}

31829b8d9f9dad469523b0a8f65408a00de6a0cb
Nina <ninawedig@yahoo.de>
2024-03-14 06:25:52 +0100

    New Changes

--- javascript/signup.js
+++ javascript/signup.js
@@ -2,50 +2,12 @@ let users = [
     { 'name': 'Nina', 'email': 'ninawedig@yahoo.de', 'password': 'passwort123' }
 ];
 
-function login() {
-    let emailInput = document.getElementById('email');
-    let passwordInput = document.getElementById('password');
-    let messageBox = document.getElementById('messagebox');
-    let messageBoxPassword = document.getElementById('messageboxPassword');
-    if (!emailInput.value) {
-        messageBox.textContent = "Please fill out this field.";
-        document.getElementById('email').classList.add('inputEmpty');
-        return; // mit return wird die Ausführung der Funktion sofort beendet, da das Feld nicht ausgefüllt ist
-    } else {
-        messageBox.textContent = "";
-    }
-
-    if (!passwordInput.value) {
-        messageBoxPassword.textContent = "Please fill out this field.";
-        document.getElementById('password').classList.add('inputEmpty');
-        return; 
-    } else {
-        messageBoxPassword.textContent = ""; 
-    }
-
-    let user = users.find(u => u.email === emailInput.value && u.password === passwordInput.value);
-    if (user) {
-        openSummaryPage(); 
-    } else {
-        messageBoxPassword.textContent = "Wrong password Ups! Try again."; 
-        document.getElementById('password').classList.add('inputEmpty');
-    }
-}
-
-
-function openSummaryPage() {
-    window.location.href = "summary.html";
-}
-
-
 function addUser() {
- 
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('confirmpassword').value;
     let policy = document.querySelector('.checkbox').checked;
-
 
     let messageBoxName = document.getElementById('messageboxName');
     let messageBoxEmail = document.getElementById('messageboxEmail');
@@ -54,17 +16,13 @@ function addUser() {
     let messageBoxPolicy = document.getElementById('messageboxPolicy');
 
     if (name && email && password && confirmPassword && policy) {
-
         if (password !== confirmPassword) {
-
             messageboxConfirmpassword.textContent = "Ups! Your password don`t match";
             document.getElementById('confirmpassword').classList.add('inputEmpty');
             return;
         } else {
             messageBoxPassword.textContent = "";
-      
         }
-
 
         users.push({ 'name': name, 'email': email, 'password': password });
         showSuccess();
@@ -74,41 +32,56 @@ function addUser() {
             document.getElementById('name').classList.add('inputEmpty');
         } else {
             messageBoxName.textContent = "";
-           
+
         }
         if (!email) {
             messageBoxEmail.textContent = "Please fill out this field.";
             document.getElementById('email').classList.add('inputEmpty');
         } else {
             messageBoxEmail.textContent = "";
-            
+
         }
         if (!password) {
             messageBoxPassword.textContent = "Please fill out this field.";
             document.getElementById('password').classList.add('inputEmpty');
         } else {
             messageBoxPassword.textContent = "";
-            
+
         }
         if (!confirmPassword) {
             messageboxConfirmpassword.textContent = "Please fill out this field.";
             document.getElementById('confirmpassword').classList.add('inputEmpty');
         } else {
             messageBoxPassword.textContent = "";
-          
+
         }
         if (!policy) {
             messageBoxPolicy.textContent = "Please confirm the Privacy policy.";
-        
+
         } else {
             messageBoxPolicy.textContent = "";
-          
         }
-       
     }
-  
-    
 }
+
+// checkField(name);
+// checkField(email);
+// checkField(password);
+// checkField(confirmPassword);
+
+// function checkField(field){
+//     if(!field) {
+//         document.getElementById(id).classList.add('inputEmpty');
+//         let idBigFirstLetter = id[0].toUpperCase() + id.slice(1);
+//         console.log(idBigFirstLetter);
+//         document.getElementById(`messagebox${idBigFirstLetter}`).textContent = ""; 
+//     }
+//     else
+// {
+//     resetOutline(field);
+// }
+// }
+
 
 function resetOutline(id) {
     document.getElementById(id).classList.remove('inputEmpty');
@@ -117,39 +90,26 @@ function resetOutline(id) {
     document.getElementById(`messagebox${idBigFirstLetter}`).textContent = "";
 }
 
-function checkPolicy(){
+function checkPolicy() {
     let checkbox = document.querySelector('.checkbox');
-    if(checkbox.checked){
+    if (checkbox.checked) {
         document.getElementById('messageboxPolicy').textContent = "";
     }
 }
-
-
-function resetOutlineLogin(id) {
-    document.getElementById(id).classList.remove('inputEmpty');
-}
-
 
 async function showSuccess() {
     document.getElementById('successBackgroud').classList.remove('successBackgroundHide');
     document.getElementById('successBackgroud').classList.add('successBackgroundShow');
     document.getElementById('successButton').classList.add('successButtonShow');
     document.getElementById('successButton').classList.remove('successButtonHide');
-
-
     const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
     await delay(2000);
-
     window.location.href = "login.html";
 }
-
-
 
 function openPrivacyPolicyPage() {
     window.location.href = "privacypolicy.html";
 }
-
 
 function openLegalNoticePage() {
     window.location.href = "legalnotice.html";

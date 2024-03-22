2cac881433e538cb15b05c4c0f295c1db264e0f4
Alex Eremie <alex.eremie19@gmail.com>
2024-03-22 13:28:16 +0100

    Update header.js

--- javascript/header.js
+++ javascript/header.js
@@ -1,7 +1,7 @@
 function renderHeader() {
     document.getElementById('header').innerHTML = `
     <div class="headerTitle">Kanban Project Management Tool</div>
-    <div class="headerLogo"><img src="./../img/favicon.png"></div>
+    <div class="headerLogo"><img src="./img/favicon.png"></div>
     <div class="headerIcons">
         <a href="help.html" class="helpIconContainer"><img class="helpIcon" src="./img/help.svg" alt=""></a>
         <div onclick="showSmallMenu()" id="activeUserButton" class="activeUser">SM</div>
